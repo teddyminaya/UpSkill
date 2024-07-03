5f5c362bed8af8c253b9a0f5ac3119302a27b5ae
Teddy Minaya <teddyminaya@gmail.com>
2024-07-02 21:31:33 -0400

    added landing markup

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './LandingPage.scss'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <div className="landing-entry">
+    <div className="landing-entry__image-container">
+      {}
+    </div>
+    <div className="landing-entry__content">
+      <h1 className="landing-entry__title">UpSkill</h1>
+      <button className="landing-entry__button">Enter</button>
+    </div>
+  </div>
+);
+};
+
+
 
 export default LandingPage
