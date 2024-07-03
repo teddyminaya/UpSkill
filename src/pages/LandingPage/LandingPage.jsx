76b9426c63dcce593e5c57a6d96ca17a2c01be73
Teddy Minaya <teddyminaya@gmail.com>
2024-07-02 22:45:50 -0400

    styled LandingPage/ added keyframes functionality for animation purposes/ added upskill athlete image to assets folder/ added new variables / added the default background color to App

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import './LandingPage.scss'
+import Athlete from "../../assets/images/UpSkill-Athlete.jpeg"
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   return (
     <div className="landing-entry">
     <div className="landing-entry__image-container">
-      {}
+    <img src={Athlete} alt="UpSkill Athelete"/>
     </div>
     <div className="landing-entry__content">
-      <h1 className="landing-entry__title">UpSkill</h1>
-      <button className="landing-entry__button">Enter</button>
+      <h1 className="landing-entry__title">Welcome to UpSkill</h1>
+      <Link to="/home">
+      <button className="landing-entry__button">Begin
+      </button>
+      </Link>
     </div>
   </div>
 );
