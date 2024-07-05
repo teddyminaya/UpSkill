aa9d0ded1a5df90898f65c13be12fb6b90fab8df
Teddy Minaya <teddyminaya@gmail.com>
2024-07-05 15:12:15 -0400

    added styling and populated backend positions json to the homepage

--- src/components/PositionCard/PositionCard.jsx
+++ src/components/PositionCard/PositionCard.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import "./PositionCard.scss";
 
 const PositionCard = ({item}) => {
   
     return (
-    <img src={`http://localhost:8000/assets/images/${item.img}`} />
+      <>
+        <div className="card">
+        <div className="card__image-container">
+          <img src={`http://localhost:8000/assets/images/${item.img}`} alt="The Athelte at their respective postion" className="card__image" />
+        </div>
+        <div className="card__content">
+          <h2 className="card__title">{item.position}</h2>
+          <p className="card__duration">{item.duration}</p>
+        </div>
+        <div className='card__button-container'>
+        <button className="card__button">Start Now</button>
+        </div>
+      </div>
+    </>
   )
 }
 
